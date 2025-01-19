bbcd486ce3b85949f087fae54c2031ff791e8dad
Mexica23 <cedricmex123@gmail.com>
2025-01-19 14:24:42 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -15,6 +15,8 @@ def main()
 # Add initial menu items
 menu.add_item("Burger", 10.99)
 menu.add_item("Fries", 4.99)
+menu.add_item("gg", 18.99)
+menu.add_item("renz", 4.99)
 
 if__name__"__main__":
 main()
