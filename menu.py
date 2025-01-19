bfb77d783a4c00d460ba0de789a1d1506edc85f2
Mexica23 <cedricmex123@gmail.com>
2025-01-19 14:16:44 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -9,7 +9,7 @@ def add_item(self,name,price):
 
 def get_price(self, name):
   return self.menu_items.get(name, None)
-renz pogi
+renz beki
 def main() 
 # Add initial menu items
 menu.add_item("Burger", 10.99)
